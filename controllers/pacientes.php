8b05a86da4638ad2ad2e5a473f8d1290a592fc7f
juan lawrie <juanlwr@yahoo.com.ar>
2017-10-09 01:33:17 -0300

    filtro para usuarios

--- controllers/pacientes.php
+++ controllers/pacientes.php
@@ -1,38 +1,91 @@
 <?php 
-	require_once("vendor/twig/twig/lib/Twig/Autoloader.php");
-	require_once "twigDatos.php";
-	require_once "models/configDatos.php";
-	require_once "models/conexionModelo.php";
-	require_once "models/pacienteModelo.php";
 	
-	$datos = datosTwig::getInstance();
-	$datos = $datos->datosConfig();
-	$plantilla = 'frontDeshabilitado.twig.html';
-	if ($datos['habilitado'] == 1 ) {
+	/**
+	* 
+	*/
+	class PacienteController extends Controller
+	{
 		
-		session_start();
-		
-		if (isset($_SESSION['usuario'])) {
-			# code...
-			/*if (no tiene permisos) { aca chequear por los permisos 
-				# code...
-				$plantilla = 'noAutorizado.twig.html';
-			} lo siguiente tendria que ir en un else*/
-			if ($_GET['action'] == 'index'){
-				$pm = new PacienteModelo();
-				$datos['pacientes'] = $pm->listar();	
-				$plantilla = 'paciente_index.twig.html';
-			}
-		}
-		else {
-			$plantilla = 'login.twig.html';
-		}
+		private static $instance;
+
+        public static function getInstance() {
+
+            if (!isset(self::$instance)) {
+                self::$instance = new self();
+            }
+
+            return self::$instance;
+        }
+        
+        private function __construct() {
+            
+        }
+	
+        public function trabajar($accion)
+        {
+         	if (!$this->estaLogueado()) 
+         	{
+         		$datos = $this->datosTwig(false);
+         		$datos['error'] = 'Esta seccion es solo para usuarios registrados';	
+         		$plantilla = 'login.twig.html';
+         	}
+         	elseif (!$this->tienePermiso("paciente_" . $accion)) 
+         	{
+         		$datos = $this->datosTwig(true);
+         		$plantilla = 'noAutorizado.twig.html';
+         	}
+         	else
+         	{
+         		$datos = $this->datosTwig(true);
+         		switch ($accion) {
+         			case 'index':
+                        $pm = new PacienteModelo();
+                        $pacientesPag = $pm->listar(isset($_GET['page']) ? $_GET['page'] : 1 );  
+                        $datos['pacientes'] = $pacientesPag->getDatos();
+                        $datos['paginationPath'] = "index.php?seccion=pacienteController&action=index&page="; 
+                        $datos['lastPage'] = $usuariosPag->getTotal();
+                        $datos['currentPage'] = $usuariosPag->getActual();
+                        $plantilla = 'usuario_index.twig.html';
+                        break;
+         			case 'new':
+         				$plantilla = 'usuario_new.twig.html';
+         				break;
+                    case 'eliminar':
+                        //var_dump($_GET['username']);
+                        $um = new UsuarioModelo();
+                        $username =  $_GET['username'];              
+                        $um->eliminar($username);
+                        $datos['usuarios'] = $um->listar();  
+                        $plantilla = 'usuario_index.twig.html';
+                        break;
+         			case 'newDB':
+         				$um = new UsuarioModelo();
+         				if ($um->yaExisteUsuario($_POST['username'])) {
+         					$datos['usuarioNuevo'] = $_POST;
+         					$datos['error'] = 'Ese nombre de usuario ya esta registrado';
+         					$plantilla = 'usuario_new.twig.html';
+         				}
+         				elseif ($um->yaExisteCorreo($_POST['email'])) {
+         					
+         					$datos['usuarioNuevo'] = $_POST;
+         					$datos['error'] = 'Ese correo electronico ya esta registrado';
+         					$plantilla = 'usuario_new.twig.html';
+         				}
+         				else{
+         					$um->insertar($_POST);
+         					$datos['usuarios'] = $um->listar();	
+							$plantilla = 'usuario_index.twig.html';
+         				}
+         				break;
+         			default:
+         				# code...
+         				
+         				break;
+         		}
+         	}
+         	$this->mostrarVista($plantilla, $datos);
+         } 
+
 	}
 
-	/*require_once("twigClass.php");
-	$twig = TwigClass::getTwig('templates');
-	$template = $twig->loadTemplate($plantilla);
-	$template->display($datos);*/
-
-
- ?>
+?>
--- models/listables.php
+++ models/listables.php
@@ -11,26 +11,26 @@
 			parent::__construct();
 		}
 	
-		public function getLimitOffset($table, $pagina)
+		public function getLimitOffset($table, $pagina, $where, $args )
 		{
 			$dc = new DatosConfig();
 			(int)$limite = $dc->paginas();
 			(int)$offset = ($pagina - 1) * $limite;
-			$sql = "SELECT * FROM $table WHERE borrado = 0 " ;
+			$sql = "SELECT * FROM $table WHERE borrado = 0 " . $where ;
 			$consulta = $this->base->prepare($sql);
-			$total = $consulta->execute(); 
+			$total = $consulta->execute($args); 
 			$total = $consulta->rowCount();
 			$paginasTotales = ceil($total / $limite);
 			$pp = new ParametrosPag($limite, $offset, $paginasTotales);
 			return $pp;
 		}
 
-		public function getDatosPara($table, $limit, $offset)
+		public function getDatosPara($table, $limit, $offset, $where, $args)
 		{
-			$sql = "SELECT * FROM $table WHERE borrado = 0 LIMIT $offset, $limit " ;
+			$sql = "SELECT * FROM $table WHERE borrado = 0 ". $where ." LIMIT $offset, $limit " ;
 			$this->base->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 			$consulta = $this->darConexion()->prepare($sql);
-			$consulta->execute();
+			$consulta->execute($args);
 			return $consulta->fetchAll();
 		}
 	}
