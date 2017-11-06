5d35ad15ef7d5d482fe2bb44907193a7494c1645
juan lawrie <juanlwr@yahoo.com.ar>
2017-11-06 20:42:09 -0300

    api con el get turnos funcionando. para usarla es /api/index.php/dd-mm-aaaa

---
+++ api/index.php
@@ -0,0 +1,56 @@
+<?php 
+	require_once '../vendor/autoload.php';
+	require_once '../models/conexionModelo.php';
+	require_once '../models/turnosModelo.php';
+	require_once '../models/listables.php';
+
+	use \Psr\Http\Message\ServerRequestInterface as Request;
+	use \Psr\Http\Message\ResponseInterface as Response;
+
+	$config = [
+	    'settings' => [
+	        'displayErrorDetails' => true,
+	    ],
+	];
+	
+	$app = new \Slim\App($config);
+	
+	$app->get('/turnos/{fecha}', function ($request, $response, $args) {
+	   	//$response->withStatus(200);
+		//$res->setBody(json_encode($meses));
+		$turnosM = new TurnosModelo();
+		if (!$turnosM->verificarFecha($args['fecha'])) {
+			$response->withHeader('Content-Type', 'text/plain');
+			$response->getBody()->write("Formato de fecha no valido. ");
+			$response->getBody()->write("El formato de fecha debe ser dd-mm-aaaa");
+			return $response;
+		}
+		
+		return $response->withJson($turnosM->turnos($args['fecha']), 200);
+	});
+
+	$app->post('/turnos', function ($request, $response, $args) {
+		   	
+		   	$data = $request->getParsedBody();
+		   	$dni = $data['dni'];
+		   	$fecha = $data['fecha'];
+		   	$hora = $data['hora'];
+		  	$turnosM = new TurnosModelo();
+			$response->withHeader('Access-Control-Allow-Origin', "*");
+			$response->withHeader('Content-Type', 'text/plain');
+			if (!$turnosM->verificarFecha($fecha) || !$turnosM->verificarHora($hora)) {
+				$response->getBody()->write("Formato de fecha o de hora no son validos. ");
+				$response->getBody()->write("El formato de fecha debe ser dd-mm-aaaa y el de la hora debe ser hh:mm entre las 8 y las 20");
+				return $response;
+			}
+			$idturno = $turnosM->reservar($dni, $fecha, $hora);
+			if ($idturno == 0) {
+				$response->getBody()->write("Su turno no se pudo reservar.");
+				return $response;	
+			}
+			$response->getBody()->write("Su identificacion de turno es " . $idturno);
+			return $response; 	
+	});
+	
+	$app->run();
+ ?>
