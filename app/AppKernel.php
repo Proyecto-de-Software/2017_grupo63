ef35014fe85e78322402044f3e5ebe296fb07088
juanlwr <juanlwr@yahoo.com.ar>
2018-05-21 15:10:08 -0300

    probando

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,7 +22,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('prod', false))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
